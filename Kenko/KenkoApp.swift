30fa5da8bd79d6d94d5929ba2f2038b0ae043f9d
Ryan Wirth <ryan@ryanwirth.ca>
2021-02-14 17:03:02 -0700

    Initial Commit

---
+++ Kenko/KenkoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KenkoApp.swift
+//  Kenko
+//
+//  Created by Ryan Wirth on 2021-02-14.
+//
+
+import SwiftUI
+
+@main
+struct KenkoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
