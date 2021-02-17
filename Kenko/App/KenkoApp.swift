0d47cd392b0f787655a8d87706ccfb009d410fce
Ryan Wirth <ryan@ryanwirth.ca>
2021-02-17 16:14:23 -0700

    Configure Firebase

--- Kenko/App/KenkoApp.swift
+++ Kenko/App/KenkoApp.swift
@@ -6,13 +6,23 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct KenkoApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             // TODO: Replace with MainView
             OnboardingView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
     }
 }
