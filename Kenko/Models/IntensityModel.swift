bd936a92e0d4958251a9e42a6d15145278118b7a
Ryan Wirth <ryan@ryanwirth.ca>
2021-02-28 14:29:20 -0700

    Create IntensityModel

---
+++ Kenko/Models/IntensityModel.swift
@@ -0,0 +1,12 @@
+//
+//  IntensityModel.swift
+//  Kenko
+//
+//  Created by Ryan Wirth on 2021-02-28.
+//
+
+import Foundation
+
+enum IntensityModel: String, Codable {
+    case heavy, light, none
+}
